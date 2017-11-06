a9288f92696827fe8e788d2f42273800e448b398
tacostacostacos <david.epstein@libertymutual.com>
2017-11-06 12:05:54 -0500

    Updated the scripts for updating on a site level
    Updated the sh scripts

--- scripts/sites/destroy.sh
+++ scripts/sites/destroy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/sites"
+TOKEN=gNrwGnmXckEr56LnSyqzsUbEAeSxBh19qyJ+yYN/6us=--VmVUG8at2lC6fEuhrhp0x3lKIwVnNq2QCuEv7Dd5uo8=
+ID=5a0093363b2f1a0bfdb735cf
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
--- scripts/sites/update.sh
+++ scripts/sites/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/sites"
+TOKEN=gNrwGnmXckEr56LnSyqzsUbEAeSxBh19qyJ+yYN/6us=--VmVUG8at2lC6fEuhrhp0x3lKIwVnNq2QCuEv7Dd5uo8=
+ID=5a0093363b2f1a0bfdb735cf
+PAGETITLE="Julies Blog"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +12,8 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "site": {
+      "title": "'"${PAGETITLE}"'"
     }
   }'
 
